53bf6e06664f0f1ad1c9cdb5b8e3c22da789bc14
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2023-06-29 16:51:31 -0300

    Update header.py

--- factory/header.py
+++ factory/header.py
@@ -3,5 +3,5 @@
 
 def json_header():
     return{
-        "Authorization":"Bearer tfp_EBhN4qzghJhsSLxKTLfsyeAdAtaPRJiEFvfcW9pEe2wH_fYfvU178YuiH"
+        "Authorization":"Bearer TYPEFORM_ACCESS_TOKEN"
     }
